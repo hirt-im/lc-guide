9d60b628bb8b7c096cce207a73cd217b58cd34a8
Michael Hirt <hirtm14@gmail.com>
2023-06-21 11:49:47 -0700

    Edit styling

--- src/components/ProblemList.jsx
+++ src/components/ProblemList.jsx
@@ -3,23 +3,25 @@ import { problems } from '../data/problems';
 
 export default function ProblemList(props){
     return(
-        <table className='problem-table'>
-            <thead>
-                <tr>
-                    <th>Problem</th>
-                    <th>Difficulty</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Object.keys(problems[props.category]).map((key) => {
-                    return (
-                        <tr>
-                            <td>{key}</td>
-                            <td>{problems[props.category][key].difficulty}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <div className='table-container'>
+            <table className='problem-table'>
+                <thead>
+                    <tr>
+                        <th>Problem</th>
+                        <th>Difficulty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(problems[props.category]).map((key) => {
+                        return (
+                            <tr>
+                                <td>{key}</td>
+                                <td>{problems[props.category][key].difficulty}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
