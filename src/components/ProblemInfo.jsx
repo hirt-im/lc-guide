7042f657ffe553cf981b7aa1adc32c2132798ad6
Michael Hirt <hirtm14@gmail.com>
2023-06-21 12:57:46 -0700

    Edit styling

--- src/components/ProblemInfo.jsx
+++ src/components/ProblemInfo.jsx
@@ -1,12 +1,14 @@
 import { problems } from "../data/problems";
+import './ProblemInfo.css';
 
 export default function ProblemInfo(props){
     let problem = problems[props.category][props.problem]
     let problemName = props.problem
 
     return (
-        <div className='problem-info-container'>{problemName}
-            <div>{problem.difficulty}</div>
+        <div className='problem-info-container'>
+            <h1>{problemName}</h1>
+            <h4>{problem.difficulty}</h4>
         </div>
     );
 }
