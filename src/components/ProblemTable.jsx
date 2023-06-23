4dab30d3003b2f8ce874563c1aef2bad3fc87355
Michael Hirt <hirtm14@gmail.com>
2023-06-23 12:46:24 -0700

    Change icon color and hover color

--- src/components/ProblemTable.jsx
+++ src/components/ProblemTable.jsx
@@ -48,7 +48,7 @@ export default function ProblemTable(props){
                             // <tr className={props.problemSet[props.category][key].difficulty}>
                             <tr>
                                 <td className='fav-column' style={{ textAlign: 'center', verticalAlign: 'middle' }} >
-                                    <FaStar className='fav-icon' onClick={favoriteProblem} color={(props.problemSet[props.category][key].favorited ? 'rgb(255, 230, 0)' : null)} size='60%'/>
+                                    <FaStar className='fav-icon' onClick={favoriteProblem} color={(props.problemSet[props.category][key].favorited ? 'rgb(255, 212, 23)' : null)} size='60%'/>
                                 </td>
 
                                 <td className={props.problemSet[props.category][key].difficulty + ' ' + 'problem-column'} onClick={selectProblem}>
