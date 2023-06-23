045c0fa63693ca66a860684be3f12cac6d304246
Michael Hirt <hirtm14@gmail.com>
2023-06-23 11:18:45 -0700

    Add icons

--- src/components/ProblemTable.jsx
+++ src/components/ProblemTable.jsx
@@ -1,12 +1,25 @@
 import './ProblemTable.css';
 import { problems } from '../data/problems';
+import { FaStar } from 'react-icons/fa';
+import { BsFillCheckSquareFill } from 'react-icons/bs';
+
 
 export default function ProblemTable(props){
+
     function selectProblem(e){
         console.log(e.target.innerText);
         props.setProblem(e.target.innerText)
         // props.setProblem
     }
+
+    function favoriteProblem(e){
+        return;
+    }
+
+    function checkProblem(e){
+        return;
+    }
+
     return(
         <div className='table-container'>
             <table className='problem-table'>
@@ -20,7 +33,9 @@ export default function ProblemTable(props){
                     {Object.keys(problems[props.category]).map((key) => {
                         return (
                             <tr className={problems[props.category][key].difficulty}>
+                                <td className='fav-column' onClick={favoriteProblem}><FaStar className='fav-icon' /></td>
                                 <td className='problem-column' onClick={selectProblem}>{key}</td>
+                                <td className='check-column' onClick={checkProblem}><BsFillCheckSquareFill className='check-icon'/></td>
                                 {/* <td className='diff-col'>{problems[props.category][key].difficulty}</td> */}
                             </tr>
                         )
