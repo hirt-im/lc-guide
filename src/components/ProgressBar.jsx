6d1402761156f7532118ac2742f4b222be31ec1f
Michael Hirt <hirtm14@gmail.com>
2023-06-24 13:59:08 -0700

    Add progress bar component

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,6 +3,11 @@ import './ProgressBar.css';
 
 export default function ProgressBar(props){
     return(
-        <div className='progress-container'>{props.checked[0]}/{props.checked[1]}</div>
+        <div className='progress-container'>
+            <div className='progress-value'>
+                {props.checked[0]}/{props.checked[1]}
+            </div>
+            <progress className='progress-bar' value={props.checked[0]} max={props.checked[1]}></progress>
+        </div>
     );
 }
