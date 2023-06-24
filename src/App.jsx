f62f20b331c36a88d05236c17bd95d7111d052a9
Michael Hirt <hirtm14@gmail.com>
2023-06-24 14:39:46 -0700

    Add function to get array of favorited problems

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,22 @@ function App() {
   const [problem, setProblem] = useState(null)
   const [problemSet, setProblemSet] = useState((localStorage.getItem('problemSet') == null ? problems : JSON.parse(localStorage.getItem('problemSet'))));
 
+  let favorites = [];
+  function getFavorites(problemSet){
+    Object.keys(problemSet).forEach(category => {
+        Object.keys(problemSet[category]).forEach(problem => {
+          console.log(problemSet[category][problem])
+          let currProb = problemSet[category][problem];
+          if (currProb.favorited){
+            favorites.push(currProb);
+          }
+        })
+    })
+    console.log(favorites);
+}
+getFavorites(problemSet);
+  
+
   return (
     <div className='container'>
       <CategoriesBar 
