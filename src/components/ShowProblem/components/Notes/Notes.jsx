cc100ac0198c8a93ab9803f2dc0391fecd3e0911
Michael Hirt <hirtm14@gmail.com>
2023-06-28 12:33:19 -0700

    Add adding and removing of notes

--- src/components/ShowProblem/components/Notes/Notes.jsx
+++ src/components/ShowProblem/components/Notes/Notes.jsx
@@ -1,6 +1,7 @@
 import { IoMdAdd } from 'react-icons/io';
 import { RiSubtractLine } from 'react-icons/ri';
 import './Notes.css';
+import { useState } from 'react';
 
 
 
@@ -13,25 +14,45 @@ let testNotes = ['this is the first test bullet point',
 
 export default function Notes(props){
 
+    const [notes, setNotes] = useState(props.problem.notes)
+
     
-    function handleChange(e){
+    function editNote(e){
         props.problem.notes[e.target.id] = e.target.innerText
         localStorage.setItem('problemSet', JSON.stringify(props.problemSet));
+    }
+
+    function addNote(){
+        let newNotes = [...props.problem.notes]
+        newNotes.push('Edit notes')
+        props.problem.notes = newNotes;
+        localStorage.setItem('problemSet', JSON.stringify(props.problemSet));
+        setNotes(newNotes);
+        console.log(props.problem.notes)
+    }
+
+    function removeNote(){
+        let newNotes = [...props.problem.notes]
+        newNotes.pop();
+        props.problem.notes = newNotes;
+        localStorage.setItem('problemSet', JSON.stringify(props.problemSet));
+        setNotes(newNotes);
+        console.log(props.problem.notes)
     }
 
     return(
         <div className='notes-container'>
             <ul className='notes-list'>
                 {props.problem.notes.map((bullet, index) => (
-                    <li id={index} contenteditable="true" onInput={handleChange}>{bullet}</li>
+                    <li id={index} contenteditable="true" onInput={editNote}>{bullet}</li>
                 ))}
             </ul>
 
             <div className='button-group'>
-                <button className='add-button'>
+                <button className='add-button' onClick={addNote}>
                     <IoMdAdd />
                 </button>
-                <button className='delete-button'>
+                <button className='delete-button' onClick={removeNote}>
                     <RiSubtractLine />
                 </button>
             </div>
